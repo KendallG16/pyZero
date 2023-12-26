285ea242298cb757d324a1e16fbb116f20f63109
kendall <kendallgomezmolina16@gmail.com>
2023-12-26 17:42:55 -0600

    list names

---
+++ list.py
@@ -0,0 +1,6 @@
+names = ["Kendall", "Kylie", "Kris", "Caitlyn", "Kim", "Khloe", "Kourtney", "Rob", "Kanye", "North", "Saint", "Chicago", "Psalm"]
+
+def print_list():
+    for i in names:
+        print (i)
+print_list()
