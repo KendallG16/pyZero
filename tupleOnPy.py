14eb50cc9feb5334097e4ddd72ffa8bb0d8220bd
kendall <kendallgomezmolina16@gmail.com>
2023-12-29 07:53:42 -0600

    Set's on python the sets are unordered

--- tupleOnPy.py
+++ tupleOnPy.py
@@ -10,4 +10,9 @@ for i in names:
 
 #this is not recomended, but is you need to change the values of a tuple, you can do this:
 names = list(names)
-
+list_numbers = []
+numbers = (1,2,3,4,5,6,7,8,9,10)
+for i in numbers:
+    if i < 5:
+        list_numbers.append(i)
+        print(list_numbers) 
